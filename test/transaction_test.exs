3b142a7310f70795e7c4776f7b3d2b57596b3fa5
anipmehta <anipmehta13@gmail.com>
2018-11-25 17:26:24 -0500

    adding more test case to transaction

--- test/transaction_test.exs
+++ test/transaction_test.exs
@@ -20,15 +20,28 @@ defmodule TransactionTest do
    #User a sends $50 to user b signing with its public_key
    #User b falsely claims that it is he who sent the money to himself
    #We are able to verify that the transaction was not signed by User b
-     test "detecting invalid transaction" do
-       {:ok, user_a_id} = User.start_link()
-       from_address = User.get_public_key(user_a_id)
-       {:ok, user_b_id} = User.start_link()
-       to_address = User.get_public_key(user_b_id)
-       amount = 50.0;
-       {:ok, txn_id} = Transaction.start_link(from_address,to_address,amount)
-       User.sign_transaction(user_a_id, txn_id)
-       assert Transaction.verify_transaction(txn_id,to_address) == false
-      end
+    test "detecting invalid transaction when verifying with the to_address" do
+      {:ok, user_a_id} = User.start_link()
+      user_a_address = User.get_public_key(user_a_id)
+      {:ok, user_b_id} = User.start_link()
+      user_b_address = User.get_public_key(user_b_id)
+      amount = 50.0;
+      {:ok, txn_id} = Transaction.start_link(user_a_address, user_b_address, amount)
+      User.sign_transaction(user_a_id, txn_id)
+      assert Transaction.verify_transaction(txn_id, user_b_address) == false
+    end
+
+    test "detecting invalid transaction when a different tries to verify the transaction signed by other user" do
+      {:ok, user_a_id} = User.start_link()
+      user_a_address = User.get_public_key(user_a_id)
+      {:ok, user_b_id} = User.start_link()
+      user_b_address = User.get_public_key(user_b_id)
+      amount = 50.0;
+      {:ok, user_c_id} = User.start_link()
+      user_c_address = User.get_public_key(user_c_id)
+      {:ok, txn_id} = Transaction.start_link(user_a_address, user_b_address,amount)
+      User.sign_transaction(user_a_id, txn_id)
+      assert Transaction.verify_transaction(txn_id, user_c_address) == false
+    end
 
 end
