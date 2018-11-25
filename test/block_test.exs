42d72addfcd34183cb056a1fdfc632d6809ddab1
anipmehta <anipmehta13@gmail.com>
2018-11-25 17:42:18 -0500

    adding more block tests

--- test/block_test.exs
+++ test/block_test.exs
@@ -1,8 +1,6 @@
 defmodule BlockTest do
   use ExUnit.Case
   doctest Block
-
-
    test "check hash-code with difficulty 5" do
      {:ok, pid} = Block.start_link()
      previous_hash = Block.get_previous_hash(pid)
@@ -23,4 +21,33 @@ defmodule BlockTest do
       assert String.match?(hash, ~r/^000/);
      end
 
+     test "check validty of transactions" do
+       {:ok, user_a_id} = User.start_link()
+       {:ok, user_b_id} = User.start_link()
+       user_a_address = User.get_public_key(user_a_id)
+       user_b_address = User.get_public_key(user_b_id)
+       {:ok, txn_a_id} = Transaction.start_link(user_a_address, user_b_address, 500.0)
+       User.sign_transaction(user_a_id, txn_a_id)
+       {:ok, txn_b_id} = Transaction.start_link(user_b_address, user_a_address, 200.0)
+       User.sign_transaction(user_b_id, txn_b_id)
+       transaction_list = [txn_a_id, txn_b_id]
+       {:ok, pid} = Block.start_link()
+       Block.update_transactions(pid, transaction_list)
+       assert Block.is_valid(pid) == true
+     end
+
+     test "check invalidty of transactions" do
+       {:ok, user_a_id} = User.start_link()
+       {:ok, user_b_id} = User.start_link()
+       user_a_address = User.get_public_key(user_a_id)
+       user_b_address = User.get_public_key(user_b_id)
+       {:ok, txn_a_id} = Transaction.start_link(user_a_address, user_b_address, 500.0)
+       User.sign_transaction(user_a_id, txn_a_id)
+       {:ok, txn_b_id} = Transaction.start_link(user_b_address, user_a_address, 200.0)
+       User.sign_transaction(user_a_id, txn_b_id)
+       transaction_list = [txn_a_id, txn_b_id]
+       {:ok, pid} = Block.start_link()
+       Block.update_transactions(pid, transaction_list)
+       assert Block.is_valid(pid) == false
+     end
 end
