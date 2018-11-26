d8c74341f9659e92c0b3326c250d84b284b5a94e
anipmehta <anipmehta13@gmail.com>
2018-11-25 19:14:02 -0500

    adding complex scenarios

--- test/blockchain_test.exs
+++ test/blockchain_test.exs
@@ -7,6 +7,65 @@ defmodule BlockChainTest do
     assert Enum.count(BlockChain.get_chain(block_chain_pid)) == 1
   end
 
+  test "complex transactions scenario with multiple block and mining Rewards" do
+    difficulty = 4
+    {:ok, block_chain_pid} = BlockChain.start_link()
+    {:ok, user_a_id} = User.start_link()
+    BlockChain.add_user(block_chain_pid, user_a_id)
+    user_a_address = User.get_public_key(user_a_id)
+    mining_reward = 100.0
+    BlockChain.update_mining_reward(block_chain_pid, mining_reward)
+    BlockChain.mine_pending_transactions(block_chain_pid, user_a_address, difficulty)
+    {:ok, user_b_id} = User.start_link()
+    {:ok, user_c_id} = User.start_link()
+    BlockChain.add_user(block_chain_pid, user_b_id)
+    BlockChain.add_user(block_chain_pid, user_c_id)
+    user_b_address = User.get_public_key(user_b_id)
+    user_c_address = User.get_public_key(user_c_id)
+
+
+    {:ok, txn_a_id} = Transaction.start_link(user_a_address, user_b_address, 70.0)
+    User.sign_transaction(user_a_id, txn_a_id)
+    BlockChain.add_transaction(block_chain_pid, txn_a_id)
+
+    {:ok, txn_b_id} = Transaction.start_link(user_b_address, user_c_address, 50.0)
+    User.sign_transaction(user_b_id, txn_a_id)
+    BlockChain.add_transaction(block_chain_pid, txn_b_id)
+    BlockChain.mine_pending_transactions(block_chain_pid, user_c_address, difficulty)
+
+    user_a_balance = BlockChain.get_balance(block_chain_pid, user_a_address)
+    user_b_balance = BlockChain.get_balance(block_chain_pid, user_b_address)
+    user_c_balance = BlockChain.get_balance(block_chain_pid, user_c_address)
+    IO.inspect(user_a_balance)
+    IO.inspect(user_b_balance)
+    IO.inspect(user_c_balance)
+    check1 = user_a_balance == 30.0 and user_b_balance==20.0 and user_c_balance==50.0
+
+
+    {:ok, txn_c_id} = Transaction.start_link(user_c_address, user_b_address, 10.0)
+    User.sign_transaction(user_c_id, txn_c_id)
+    BlockChain.add_transaction(block_chain_pid, txn_c_id)
+
+    {:ok, txn_d_id} = Transaction.start_link(user_a_address, user_c_address, 20.0)
+    User.sign_transaction(user_a_id, txn_a_id)
+    BlockChain.add_transaction(block_chain_pid, txn_d_id)
+
+    {:ok, txn_e_id} = Transaction.start_link(user_b_address, user_a_address, 10.0)
+    User.sign_transaction(user_b_id, txn_a_id)
+    BlockChain.add_transaction(block_chain_pid, txn_e_id)
+    BlockChain.mine_pending_transactions(block_chain_pid, user_b_address, difficulty)
+
+    user_a_balance = BlockChain.get_balance(block_chain_pid, user_a_address)
+    user_b_balance = BlockChain.get_balance(block_chain_pid, user_b_address)
+    user_c_balance = BlockChain.get_balance(block_chain_pid, user_c_address)
+    IO.inspect(user_a_balance)
+    IO.inspect(user_b_balance)
+    IO.inspect(user_c_balance)
+    check2 = user_a_balance == 20.0 and user_b_balance == 20.0 and user_c_balance == 160.0
+
+    assert check1 == true and check2 == true
+  end
+
   test "valid block chain given all the valid blocks" do
     difficulty = 2
     {:ok, block_chain_pid} = BlockChain.start_link()
