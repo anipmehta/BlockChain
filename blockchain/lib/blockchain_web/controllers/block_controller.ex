8d92eed4bb9388db0484202a1cd8113284a63579
anipmehta <anipmehta13@gmail.com>
2018-12-13 01:59:41 -0500

    starter code fix

---
+++ blockchain/lib/blockchain_web/controllers/block_controller.ex
@@ -0,0 +1,46 @@
+defmodule BlockchainWeb.BlockController do
+  use BlockchainWeb, :controller
+
+  def get_block_chain_reference() do
+    [{_, chain_pid}] = :ets.lookup(:buckets_registry, "block_chain_reference")
+    chain_pid
+  end
+
+  def create(conn, _params) do
+
+  end
+
+  def index(conn,  %{"hash" => hash}) do
+    chain_pid = get_block_chain_reference()
+    blocks = Chain.get_chain(chain_pid)
+    IO.inspect(blocks)
+    IO.inspect(hash)
+    block_pid = Enum.reduce(blocks, None, fn(pid, acc)->
+      block_hash = Block.get_hash(pid)
+      IO.inspect(block_hash)
+      IO.inspect(hash)
+      IO.inspect(block_hash==hash)
+      if block_hash == hash do pid else acc end
+    end)
+    block_transactions = Block.get_transactions(block_pid)
+    txn_details = Enum.reduce(block_transactions, [], fn txn_id, acc->
+      from = Transaction.get_from_address(txn_id)
+      to = Transaction.get_to_address(txn_id)
+      amount = Transaction.get_amount(txn_id)
+      IO.inspect(from)
+      acc ++ [{from, to, amount}]
+    end)
+    no_of_blocks_mined = Enum.reduce(block_transactions, 0, fn txn_id, acc ->
+      from = Transaction.get_from_address(txn_id)
+      if from == "miningReward" do
+        acc + 1
+      else
+        acc
+      end
+    end)
+    block_txns_amount = Block.get_transactions_amount(block_pid)
+    IO.inspect(txn_details)
+    render(conn, "index.html", hash: hash, total_amount: block_txns_amount,
+    bitcoins_mined: no_of_blocks_mined, transactions: txn_details)
+  end
+end
